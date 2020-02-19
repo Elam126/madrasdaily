b4891978f88ab860a6dd136586743f579e6d409a
Elam126 <41664953+Elam126@users.noreply.github.com>
2020-02-19 13:17:52 +0530

    19-02-2020

--- app/Http/Controllers/newsController.php
+++ app/Http/Controllers/newsController.php
@@ -7,8 +7,91 @@ use App\news;
 
 class newsController extends Controller
 {
-    public function index(){
-        $newsapp = news::all();
-        return $newsapp;
+    public function index(Request $request){
+       $newsapp = news::all();
+        //return $newsapp;
+        return \Response::json([
+            //'message' => $newsapp
+            'data' => $this->transformCollection($newsapp)
+       ], 200); 
+
+      
+    }
+
+    public function show($id){
+        $newsapp = news::find($id);
+
+        if(!$newsapp){
+            return \Response::json([
+                'error' => [
+                    'message' => 'News does not exist'
+                ]
+            ], 404);
+        }
+
+        return \Response::json([
+            'data' => $this->transform($newsapp)
+        ], 200);
+    }
+
+    private function transformCollection($newsapp){
+       return array_map([$this, 'transform'], $newsapp->toArray());
+    }
+    
+    private function transform($newsapp){
+        return [
+               'news_id' => $newsapp['id'],
+               'heading' => $newsapp['title'],
+               'news_type' => $newsapp['catagory'],
+               'news_content' => $newsapp['body']
+            ];
+    }
+
+    public function store(Request $request)
+    {
+
+        if(! $request->title){
+            return \Response::json([
+                'error' => [
+                    'message' => 'Please Provide proper value to Insert the News'
+                ]
+            ], 422);
+        }
+        $newsapp = news::create($request->all());
+
+        return \Response::json([
+                'message' => 'News Created Succesfully',
+                'data' => $this->transform($newsapp)
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        if(! $request->title){
+            return \Response::json([
+                'error' => [
+                    'message' => 'Please Provide proper value to Update the existing News'
+                ]
+            ], 422);
+        }
+
+        //$newsapp = Joke::find($id);
+        $newsapp->news_id = $request->news_id;
+        $newsapp->heading = $request->heading;
+        $newsapp->news_type = $request->news_type;
+        $newsapp->news_content =$request->news_content;
+
+
+        //$newsapp->user_id = $request->user_id;
+        $newsapp->save();
+
+        return \Response::json([
+                'message' => 'News Updated Succesfully'
+        ]);
+    }
+
+    public function destroy($news_id)
+    {
+        news::destroy($news_id);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Route::group(['middleware' => 'cors', 'prefix' => 'api'], function(){
 Route::resource('news', 'newsController');
+//});
+/*
+Route::group(['middleware' => 'api', 'prefix' => 'api'], function(){
+	Route::resource('news', 'newsController');
+});
